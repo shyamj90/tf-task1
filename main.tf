945791f5bb1ab62779a3a4a309df79decee604e0
EC2 Default User <ec2-user@ip-172-31-44-212.us-east-2.compute.internal>
2025-10-25 05:27:18 +0000

    Added main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  
+}
+
+resource "aws_instance" "my_instance_ohio" {
+  ami = "ami-0199d4b5b8b4fde0e"
+  instance_type = "t3.micro"
+  region = "us-east-2"
+  tags = {
+    Name="tf-instance-ohio"
+  }
+}
+
+resource "aws_instance" "my_instance_virginia" {
+  ami = "ami-07860a2d7eb515d9a"
+  instance_type = "t3.micro"
+  region = "us-east-1"
+  tags = {
+    Name="tf-instance-virginia"
+  }
+}
